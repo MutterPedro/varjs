f38c6ed2803dbae75100c08b377529df7e260552
mutterpedro <pedro.mutter@catho.com>
2020-03-18 20:03:22 -0300

    fixing change directory and remove logs

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,8 @@ async function run() {
     core.info(`Running ${github.context.action}`);
 
     const script = core.getInput("script");
-    const context = github.context;
 
-    core.info(execSync("echo $GITHUB_WORKSPACE").toString());
-    core.info(execSync("ls $GITHUB_WORKSPACE").toString());
-
-    execSync(`cd $GITHUB_WORKSPACE/${context.repo.repo} && npm run ${script}`);
+    execSync(`cd $GITHUB_WORKSPACE && npm run ${script}`);
   } catch (error) {
     core.setFailed(error.message);
   }
