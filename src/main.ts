b960bced5e672ebc791ffc5acbc845db099a6141
mutterpedro <pedro.mutter@catho.com>
2020-03-18 20:00:08 -0300

    fixing change directory to repo

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,10 @@ async function run() {
     const script = core.getInput("script");
     const context = github.context;
 
-    execSync("pwd");
-    execSync("ls");
-    execSync("ls /");
+    core.info(execSync("echo $GITHUB_WORKSPACE").toString());
+    core.info(execSync("ls $GITHUB_WORKSPACE").toString());
 
-    execSync(`cd ${context.repo.repo} && npm run ${script}`);
+    execSync(`cd $GITHUB_WORKSPACE/${context.repo.repo} && npm run ${script}`);
   } catch (error) {
     core.setFailed(error.message);
   }
